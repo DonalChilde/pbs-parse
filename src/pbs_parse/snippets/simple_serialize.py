8d92886f8d8f86691deb72b677b400d751fb576c
Chad Lowe <pfmsoft@gmail.com>
2024-11-15 04:23:07 -0700

    refactored simple serializer api and types

--- src/pbs_parse/snippets/simple_serialize.py
+++ src/pbs_parse/snippets/simple_serialize.py
@@ -1,16 +1,18 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import (
+    Callable,
     Generator,
     Generic,
     Iterable,
+    Optional,
     Protocol,
     Type,
     TypeVar,
 )
 from json import dump as json_dump
 from json import load as json_load
-from dataclasses import asdict
+from dataclasses import asdict, is_dataclass
 
 
 try:
@@ -26,63 +28,78 @@ SIMPLE_OBJ = TypeVar("SIMPLE_OBJ")
 
 
 class SimpleConverter(Protocol[COMPLEX_OBJ, SIMPLE_OBJ]):
-    @staticmethod
-    def to_simple(complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ: ...
-    @staticmethod
-    def from_simple(
-        simple_obj: SIMPLE_OBJ, complex_obj: Type[COMPLEX_OBJ]
-    ) -> COMPLEX_OBJ: ...
-    @staticmethod
+
+    def to_simple(self, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ: ...
+
+    def from_simple(self, simple_obj: SIMPLE_OBJ) -> COMPLEX_OBJ: ...
+
     def from_simple_gen(
-        simple_objs: Iterable[SIMPLE_OBJ], complex_obj: Type[COMPLEX_OBJ]
+        self, simple_objs: Iterable[SIMPLE_OBJ]
     ) -> Generator[COMPLEX_OBJ, None, None]: ...
-    @staticmethod
+
     def to_simple_gen(
+        self,
         complex_objs: Iterable[COMPLEX_OBJ],
     ) -> Generator[SIMPLE_OBJ, None, None]: ...
 
 
 class SimpleSerializer(Protocol[COMPLEX_OBJ]):
-    @staticmethod
-    def save_json(path_out: Path, complex_obj: COMPLEX_OBJ): ...
-    @staticmethod
-    def load_json(path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ: ...
-    @staticmethod
-    def save_yaml(path_out: Path, complex_obj: COMPLEX_OBJ): ...
-    @staticmethod
-    def load_yaml(path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ: ...
+
+    def save_json(
+        self,
+        path_out: Path,
+        complex_obj: COMPLEX_OBJ,
+        indent: int = 1,
+        overwrite: bool = False,
+    ): ...
+
+    def load_json(self, path_in: Path) -> COMPLEX_OBJ: ...
+
+    def save_yaml(
+        self,
+        path_out: Path,
+        complex_obj: COMPLEX_OBJ,
+        indent: int = 1,
+        overwrite: bool = False,
+    ): ...
+
+    def load_yaml(self, path_in: Path) -> COMPLEX_OBJ: ...
 
 
 class SimpleSerializerABC(ABC, Generic[COMPLEX_OBJ, SIMPLE_OBJ]):
-    @classmethod
+    def __init__(
+        self,
+        complex_cls: Type[COMPLEX_OBJ],
+        simple_factory: Optional[Callable[[COMPLEX_OBJ], SIMPLE_OBJ]] = None,
+        complex_factory: Optional[Callable[[SIMPLE_OBJ], COMPLEX_OBJ]] = None,
+    ) -> None:
+        super().__init__()
+        self.complex_cls = complex_cls
+        self.simple_factory = simple_factory
+        self.complex_factory = complex_factory
+
     @abstractmethod
-    def to_simple(cls, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
+    def to_simple(self, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
         pass
 
-    @classmethod
     @abstractmethod
-    def from_simple(
-        cls, obj: SIMPLE_OBJ, complex_obj: Type[COMPLEX_OBJ]
-    ) -> COMPLEX_OBJ:
+    def from_simple(self, obj: SIMPLE_OBJ) -> COMPLEX_OBJ:
         pass
 
-    @classmethod
     def from_simple_gen(
-        cls, simple_objs: Iterable[SIMPLE_OBJ], complex_obj: Type[COMPLEX_OBJ]
+        self, simple_objs: Iterable[SIMPLE_OBJ]
     ) -> Generator[COMPLEX_OBJ, None, None]:
         for item in simple_objs:
-            yield cls.from_simple(item, complex_obj)
+            yield self.from_simple(item)
 
-    @classmethod
     def to_simple_gen(
-        cls, complex_obj: Iterable[COMPLEX_OBJ]
+        self, complex_obj: Iterable[COMPLEX_OBJ]
     ) -> Generator[SIMPLE_OBJ, None, None]:
         for item in complex_obj:
-            yield cls.to_simple(item)
+            yield self.to_simple(item)
 
-    @classmethod
     def save_json(
-        cls,
+        self,
         path_out: Path,
         complex_obj: COMPLEX_OBJ,
         indent: int = 1,
@@ -91,16 +108,14 @@ class SimpleSerializerABC(ABC, Generic[COMPLEX_OBJ, SIMPLE_OBJ]):
         check_file(path_out=path_out, overwrite=overwrite)
         path_out.parent.mkdir(exist_ok=True, parents=True)
         with open(path_out, "w") as file:
-            json_dump(obj=complex_obj, fp=file, default=cls.to_simple, indent=indent)
+            json_dump(obj=complex_obj, fp=file, default=self.to_simple, indent=indent)
 
-    @classmethod
-    def load_json(cls, path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ:
+    def load_json(self, path_in: Path) -> COMPLEX_OBJ:
         with open(path_in, "r") as file:
-            return cls.from_simple(json_load(file), complex_obj)
+            return self.from_simple(json_load(file))
 
-    @classmethod
     def save_yaml(
-        cls,
+        self,
         path_out: Path,
         complex_obj: COMPLEX_OBJ,
         indent: int = 1,
@@ -111,14 +126,13 @@ class SimpleSerializerABC(ABC, Generic[COMPLEX_OBJ, SIMPLE_OBJ]):
         check_file(path_out=path_out, overwrite=overwrite)
         path_out.parent.mkdir(exist_ok=True, parents=True)
         with open(path_out, "w") as file:
-            safe_dump(data=cls.to_simple(complex_obj), stream=file, indent=indent)
+            safe_dump(data=self.to_simple(complex_obj), stream=file, indent=indent)
 
-    @classmethod
-    def load_yaml(cls, path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ:
+    def load_yaml(self, path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ:
         if not YAML:
             raise ValueError("PyYaml not found.")
         with open(path_in, "r") as file:
-            return cls.from_simple(safe_load(file), complex_obj)
+            return self.from_simple(safe_load(file))
 
 
 def check_file(path_out: Path, overwrite: bool = False) -> bool:
@@ -134,12 +148,38 @@ def check_file(path_out: Path, overwrite: bool = False) -> bool:
 
 
 class DataclassSerializer(SimpleSerializerABC[COMPLEX_OBJ, SIMPLE_OBJ]):
-    @classmethod
-    def to_simple(cls, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
-        return asdict(complex_obj)  # type: ignore
+    def __init__(
+        self,
+        complex_cls: Type[COMPLEX_OBJ],
+        simple_factory: Optional[Callable[[COMPLEX_OBJ], SIMPLE_OBJ]] = None,
+        complex_factory: Optional[Callable[[SIMPLE_OBJ], COMPLEX_OBJ]] = None,
+    ) -> None:
+        if simple_factory is None:
+            simple_factory = self._to_simple_default
+        if complex_factory is None:
+            complex_factory = self._from_simple_default
+        self.simple_factory = simple_factory
+        self.complex_factory = complex_factory
+        super().__init__(
+            complex_cls=complex_cls,
+            simple_factory=simple_factory,
+            complex_factory=complex_factory,
+        )
+        self.complex_cls = complex_cls
 
-    @classmethod
-    def from_simple(
-        cls, simple_obj: SIMPLE_OBJ, complex_obj: Type[COMPLEX_OBJ]
-    ) -> COMPLEX_OBJ:
-        return complex_obj(**simple_obj)  # type: ignore
+    def to_simple(self, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
+        return self.simple_factory(complex_obj)
+
+    def from_simple(self, simple_obj: SIMPLE_OBJ) -> COMPLEX_OBJ:
+        return self.complex_factory(simple_obj)
+
+    def _to_simple_default(self, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
+        if is_dataclass(complex_obj):
+            return asdict(complex_obj)  # type: ignore
+        else:
+            raise ValueError(
+                f"complex_obj is not a dataclass, its type name is {type(complex_obj).__name__}"
+            )
+
+    def _from_simple_default(self, simple_obj: SIMPLE_OBJ) -> COMPLEX_OBJ:
+        return self.complex_cls(**simple_obj)
--- tests/pbs_parse/test_simple_serializer.py
+++ tests/pbs_parse/test_simple_serializer.py
@@ -1,4 +1,4 @@
-from typing import Any, TypedDict
+from typing import TypedDict
 from pbs_parse.snippets.simple_serialize import DataclassSerializer
 
 from dataclasses import dataclass, field
@@ -16,10 +16,10 @@ class DCTestClassTD(TypedDict):
 
 
 def test_to_simple():
-    serializer = DataclassSerializer[DCTestClass, DCTestClassTD]
+    serializer = DataclassSerializer[DCTestClass, DCTestClassTD](DCTestClass)
     dc = DCTestClass(name="bob", items=["chair", "table"])
     dctd: DCTestClassTD = {"name": "bob", "items": ["chair", "table"]}
     simplified_dc = serializer.to_simple(dc)
     assert simplified_dc == dctd
-    recomplicated_dc = serializer.from_simple(simplified_dc, DCTestClass)
+    recomplicated_dc = serializer.from_simple(simplified_dc)
     assert recomplicated_dc == dc
