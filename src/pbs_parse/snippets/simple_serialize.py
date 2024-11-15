cd4e346cc4e9cf911d9abfd06a09e8fc81e9b864
Chad Lowe <pfmsoft@gmail.com>
2024-11-14 18:54:45 -0700

    refactor arg names and generic names

--- src/pbs_parse/snippets/simple_serialize.py
+++ src/pbs_parse/snippets/simple_serialize.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Any, Dict, Generator, Generic, Iterable, Protocol, TypeVar
+from typing import (
+    Generator,
+    Generic,
+    Iterable,
+    Protocol,
+    Type,
+    TypeVar,
+)
 from json import dump as json_dump
 from json import load as json_load
 from dataclasses import asdict
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from _typeshed import DataclassInstance
 
 try:
     from yaml import safe_load, safe_dump
@@ -17,82 +21,104 @@ except ModuleNotFoundError:
     YAML = False
 
 
-T = TypeVar("T")
-E = TypeVar("E")
-F = TypeVar("F", bound=DataclassInstance)
-G = TypeVar("G", bound=Dict[str, Any])
+COMPLEX_OBJ = TypeVar("COMPLEX_OBJ")
+SIMPLE_OBJ = TypeVar("SIMPLE_OBJ")
 
 
-class SimpleConverter(Protocol[T, E]):
+class SimpleConverter(Protocol[COMPLEX_OBJ, SIMPLE_OBJ]):
     @staticmethod
-    def to_simple(obj: T) -> E: ...
+    def to_simple(complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ: ...
     @staticmethod
-    def from_simple(obj: E, new_obj: T) -> T: ...
+    def from_simple(
+        simple_obj: SIMPLE_OBJ, complex_obj: Type[COMPLEX_OBJ]
+    ) -> COMPLEX_OBJ: ...
     @staticmethod
-    def from_simple_gen(obj: Iterable[E], new_obj: T) -> Generator[T, None, None]: ...
+    def from_simple_gen(
+        simple_objs: Iterable[SIMPLE_OBJ], complex_obj: Type[COMPLEX_OBJ]
+    ) -> Generator[COMPLEX_OBJ, None, None]: ...
     @staticmethod
-    def to_simple_gen(obj: Iterable[T]) -> Generator[E, None, None]: ...
+    def to_simple_gen(
+        complex_objs: Iterable[COMPLEX_OBJ],
+    ) -> Generator[SIMPLE_OBJ, None, None]: ...
 
 
-class SimpleSerializer(Protocol[T]):
+class SimpleSerializer(Protocol[COMPLEX_OBJ]):
     @staticmethod
-    def save_json(path_out: Path, obj: T): ...
+    def save_json(path_out: Path, complex_obj: COMPLEX_OBJ): ...
     @staticmethod
-    def load_json(path_in: Path, new_obj: T) -> T: ...
+    def load_json(path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ: ...
     @staticmethod
-    def save_yaml(path_out: Path, obj: T): ...
+    def save_yaml(path_out: Path, complex_obj: COMPLEX_OBJ): ...
     @staticmethod
-    def load_yaml(path_in: Path, new_obj: T) -> T: ...
+    def load_yaml(path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ: ...
 
 
-class SimpleSerializerABC(ABC, Generic[T, E]):
+class SimpleSerializerABC(ABC, Generic[COMPLEX_OBJ, SIMPLE_OBJ]):
     @classmethod
     @abstractmethod
-    def to_simple(cls, obj: T) -> E: ...
-    @classmethod
-    @abstractmethod
-    def from_simple(cls, obj: E, new_obj: T) -> T: ...
-    @classmethod
-    def from_simple_gen(cls, obj: Iterable[E], new_obj: T) -> Generator[T, None, None]:
-        for item in obj:
-            yield cls.from_simple(item, new_obj)
+    def to_simple(cls, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
+        pass
 
     @classmethod
-    def to_simple_gen(cls, obj: Iterable[T]) -> Generator[E, None, None]:
-        for item in obj:
+    @abstractmethod
+    def from_simple(
+        cls, obj: SIMPLE_OBJ, complex_obj: Type[COMPLEX_OBJ]
+    ) -> COMPLEX_OBJ:
+        pass
+
+    @classmethod
+    def from_simple_gen(
+        cls, simple_objs: Iterable[SIMPLE_OBJ], complex_obj: Type[COMPLEX_OBJ]
+    ) -> Generator[COMPLEX_OBJ, None, None]:
+        for item in simple_objs:
+            yield cls.from_simple(item, complex_obj)
+
+    @classmethod
+    def to_simple_gen(
+        cls, complex_obj: Iterable[COMPLEX_OBJ]
+    ) -> Generator[SIMPLE_OBJ, None, None]:
+        for item in complex_obj:
             yield cls.to_simple(item)
 
     @classmethod
     def save_json(
-        cls, path_out: Path, obj: T, indent: int = 1, overwrite: bool = False
+        cls,
+        path_out: Path,
+        complex_obj: COMPLEX_OBJ,
+        indent: int = 1,
+        overwrite: bool = False,
     ):
         check_file(path_out=path_out, overwrite=overwrite)
         path_out.parent.mkdir(exist_ok=True, parents=True)
         with open(path_out, "w") as file:
-            json_dump(obj=obj, fp=file, default=cls.to_simple, indent=indent)
+            json_dump(obj=complex_obj, fp=file, default=cls.to_simple, indent=indent)
 
     @classmethod
-    def load_json(cls, path_in: Path, new_obj: T) -> T:
+    def load_json(cls, path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ:
         with open(path_in, "r") as file:
-            return cls.from_simple(json_load(file), new_obj)
+            return cls.from_simple(json_load(file), complex_obj)
 
     @classmethod
     def save_yaml(
-        cls, path_out: Path, obj: T, indent: int = 1, overwrite: bool = False
+        cls,
+        path_out: Path,
+        complex_obj: COMPLEX_OBJ,
+        indent: int = 1,
+        overwrite: bool = False,
     ):
         if not YAML:
             raise ValueError("PyYaml not found.")
         check_file(path_out=path_out, overwrite=overwrite)
         path_out.parent.mkdir(exist_ok=True, parents=True)
         with open(path_out, "w") as file:
-            safe_dump(data=cls.to_simple(obj), stream=file, indent=indent)
+            safe_dump(data=cls.to_simple(complex_obj), stream=file, indent=indent)
 
     @classmethod
-    def load_yaml(cls, path_in: Path, new_obj: T) -> T:
+    def load_yaml(cls, path_in: Path, complex_obj: Type[COMPLEX_OBJ]) -> COMPLEX_OBJ:
         if not YAML:
             raise ValueError("PyYaml not found.")
         with open(path_in, "r") as file:
-            return cls.from_simple(safe_load(file), new_obj)
+            return cls.from_simple(safe_load(file), complex_obj)
 
 
 def check_file(path_out: Path, overwrite: bool = False) -> bool:
@@ -107,11 +133,13 @@ def check_file(path_out: Path, overwrite: bool = False) -> bool:
     return True
 
 
-class DataclassSerializer(SimpleSerializerABC[F, G]):
+class DataclassSerializer(SimpleSerializerABC[COMPLEX_OBJ, SIMPLE_OBJ]):
     @classmethod
-    def to_simple(cls, obj: F) -> G:
-        return asdict(obj)  # type: ignore
+    def to_simple(cls, complex_obj: COMPLEX_OBJ) -> SIMPLE_OBJ:
+        return asdict(complex_obj)  # type: ignore
 
     @classmethod
-    def from_simple(cls, obj: G, new_obj: F) -> F:
-        return new_obj(**obj)  # type: ignore
+    def from_simple(
+        cls, simple_obj: SIMPLE_OBJ, complex_obj: Type[COMPLEX_OBJ]
+    ) -> COMPLEX_OBJ:
+        return complex_obj(**simple_obj)  # type: ignore
