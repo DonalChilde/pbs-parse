5a0619ccc17a4a62ac56b7c5712b011395776b5f
Chad Lowe <pfmsoft@gmail.com>
2024-12-13 05:25:19 -0700

    add expanded to data generation.

--- scripts/manage_test_data.py
+++ scripts/manage_test_data.py
@@ -9,12 +9,19 @@ from typing import Annotated
 
 import typer
 
-from pbs_parse.cli import parse_trips_cli as parse
-from pbs_parse.cli import split_pages_cli, split_trips_cli
-from pbs_parse.cli import structure_trips_cli as structure
+from pbs_parse.cli import (
+    expand_trips_cli,
+    parse_trips_cli,
+    split_pages_cli,
+    split_trips_cli,
+    structure_trips_cli,
+)
 
 # typer ./scripts/manage_test_data.py run parse-source-all ~/projects/tmp/pbs-data/2024.11.01-2024.12.01 2024-11-01 2024-12-01
 # typer ./scripts/manage_test_data.py run reset-source-all ~/projects/tmp/pbs-data/2024.11.01-2024.12.01
+
+# typer ./scripts/manage_test_data.py run expand-all ~/projects/tmp/pbs-data/2024.11.01-2024.12.01
+# typer ./scripts/manage_test_data.py run reset-expanded-all ~/projects/tmp/pbs-data/2024.11.01-2024.12.01
 
 app = typer.Typer()
 logger = logging.getLogger(__name__)
@@ -36,36 +43,44 @@ def output_base_dir_from_source_file(source_path: Path) -> Path:
     Data store layout assumes a subdirectory based on the name of the source
     file, without the file suffix, in the same directory as the source file.
     """
-    output_dir = source_path.parent / source_path.stem
-    return output_dir
+    return source_path.parent / source_path.stem
 
 
 def parsed_trip_dir_from_source_file(source_file: Path) -> Path:
     """Get the directory for parsed trips, based on the source .txt file."""
-    parsed_dir = output_base_dir_from_source_file(source_file)
-    page_dir = parsed_dir / "parsed"
-    return page_dir
+    base_dir = output_base_dir_from_source_file(source_file)
+    return base_dir / "parsed"
+
+
+def expanded_trip_dir_from_source_file(source_file: Path) -> Path:
+    """Get the directory for expanded trips, base on the source .txt file."""
+    base_dir = output_base_dir_from_source_file(source_file)
+    return base_dir / "expanded"
 
 
 def structured_trip_dir_from_source_file(source_file: Path) -> Path:
     """Get the directory for structured trips, based on the source .txt file."""
-    parsed_dir = output_base_dir_from_source_file(source_file)
-    page_dir = parsed_dir / "structured"
-    return page_dir
+    base_dir = output_base_dir_from_source_file(source_file)
+    return base_dir / "structured"
 
 
 def split_trip_dir_from_source_path(source_file: Path) -> Path:
     """Get the directory for split trips, based on the source .txt file."""
-    parsed_dir = output_base_dir_from_source_file(source_file)
-    trip_dir = parsed_dir / "split_trips"
-    return trip_dir
+    base_dir = output_base_dir_from_source_file(source_file)
+    return base_dir / "split_trips"
 
 
 def split_page_dir_from_source_file(source_file: Path) -> Path:
     """Get the directory for split pages, based on the source .txt file."""
-    parsed_dir = output_base_dir_from_source_file(source_file)
-    page_dir = parsed_dir / "split_pages"
-    return page_dir
+    base_dir = output_base_dir_from_source_file(source_file)
+    return base_dir / "split_pages"
+
+
+def remove_expanded_trip_directory(source_file: Path):
+    """Remove the expanded trip directory, based on the source .txt file."""
+    structured_dir = expanded_trip_dir_from_source_file(source_file)
+    if structured_dir.is_dir():
+        shutil.rmtree(structured_dir)
 
 
 def remove_structured_trip_directory(source_file_path: Path):
@@ -107,28 +122,38 @@ def remove_output_directory(source_file_path: Path):
         shutil.rmtree(parsed_dir)
 
 
+def expand_trips(source_path: Path, overwrite: bool):
+    """Expand structured trips base on the source .txt file."""
+    jobs = expand_trips_cli.build_jobs_from_directory(
+        path_in=structured_trip_dir_from_source_file(source_path),
+        path_out=expanded_trip_dir_from_source_file(source_path),
+        overwrite=overwrite,
+    )
+    expand_trips_cli.expand_trips_rich(jobs=jobs)
+
+
 def structure_trips(
     source_path: Path, effective_from: date, effective_to: date, overwrite: bool
 ):
     """Structure parsed trips based on the source .txt file."""
-    jobs = structure.build_jobs_from_directory(
+    jobs = structure_trips_cli.build_jobs_from_directory(
         path_in=parsed_trip_dir_from_source_file(source_path),
         path_out=structured_trip_dir_from_source_file(source_path),
         effective_from=effective_from,
         effective_to=effective_to,
         overwrite=overwrite,
     )
-    structure.structure_trips_rich(jobs=jobs)
+    structure_trips_cli.structure_trips_rich(jobs=jobs)
 
 
 def parse_trips(source_path: Path, overwrite: bool):
     """Parse split trips based on the source .txt file."""
-    jobs = parse.build_jobs_from_directory(
+    jobs = parse_trips_cli.build_jobs_from_directory(
         path_in=split_trip_dir_from_source_path(source_path),
         path_out=parsed_trip_dir_from_source_file(source_path),
         overwrite=overwrite,
     )
-    parse.parse_trips_rich(jobs=jobs)
+    parse_trips_cli.parse_trips_rich(jobs=jobs)
 
 
 def split_pages(source_path: Path, overwrite: bool):
@@ -189,6 +214,7 @@ def parse_source(
         effective_to=effective_to.date(),
         overwrite=overwrite,
     )
+    expand_trips(source_path=path_in, overwrite=overwrite)
 
 
 @app.command()
@@ -238,6 +264,60 @@ def parse_source_all(
             effective_to=effective_to.date(),
             overwrite=overwrite,
         )
+    for file in files:
+        expand_trips(source_path=file, overwrite=overwrite)
+
+
+@app.command()
+def reset_expanded_all(
+    ctx: typer.Context,
+    path_in: Annotated[
+        Path,
+        typer.Argument(
+            help="Directory of source txt files.",
+            exists=True,
+            file_okay=False,
+            dir_okay=True,
+        ),
+    ],
+):
+    glob = "*.txt"
+    typer.echo("\nParsing Bid Packages.....")
+    typer.echo(f"Looking for files in {path_in}")
+    files = [f for f in path_in.glob(glob) if f.is_file()]
+    typer.echo(f"Found {len(files)} files")
+    if not files:
+        raise typer.BadParameter(
+            f"No files found in directory. files are expected to match {glob}"
+        )
+    for file in files:
+        remove_expanded_trip_directory(source_file=file)
+
+
+@app.command()
+def expand_all(
+    ctx: typer.Context,
+    path_in: Annotated[
+        Path,
+        typer.Argument(
+            help="Directory of source txt files.",
+            exists=True,
+            file_okay=False,
+            dir_okay=True,
+        ),
+    ],
+):
+    glob = "*.txt"
+    typer.echo("\nParsing Bid Packages.....")
+    typer.echo(f"Looking for files in {path_in}")
+    files = [f for f in path_in.glob(glob) if f.is_file()]
+    typer.echo(f"Found {len(files)} files")
+    if not files:
+        raise typer.BadParameter(
+            f"No files found in directory. files are expected to match {glob}"
+        )
+    for file in files:
+        expand_trips(source_path=file, overwrite=False)
 
 
 @app.command()
