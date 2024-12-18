3f99b91bbbc88ae246673a66c8e0519ddfe6ceec
Chad Lowe <pfmsoft@gmail.com>
2024-12-18 09:53:08 -0700

    remove unused model

--- src/pbs_parse/pbs_2022_01/models/validation.py
+++ src/pbs_parse/pbs_2022_01/models/validation.py
@@ -1,10 +1,8 @@
 """Models used in validating trips."""
 
-from dataclasses import asdict, dataclass, field
+from dataclasses import dataclass, field
 from datetime import date
-from pprint import pformat
 
-from .expanded import ExpandedTrip
 from .parsed_trip import ParsedTrip
 from .structured import StructuredTrip
 
@@ -26,23 +24,4 @@ class StructuredValidation:
             f"parsed_path: {self.parsed_path}\nstructured path: {self.structured_path}\n"
             f"external_start_dates: {self.external_start_dates!r}\n"
             f"{self.parsed_trip}\n\n{self.structured_trip}\n\n{"\n".join(self.errors)}"
-        )
-
-
-@dataclass(slots=True, kw_only=True)
-class ExpandedValidation:
-    """A container for all the information required to validate an expanded trip."""
-
-    errors: list[str] = field(default_factory=list)
-    expanded_trip: ExpandedTrip
-    structured_trip: StructuredTrip
-    expanded_path: str
-    structured_path: str
-
-    def __str__(self) -> str:
-        """Custom str output."""
-        return (
-            f"Errors:/n{"\n".join(self.errors)}"
-            f"\nexpanded_path: {self.expanded_path}\nstructured path: {self.structured_path}\n"
-            f"{self.expanded_trip}\n\n{pformat(asdict(self.structured_trip),sort_dicts=False)}"
         )
