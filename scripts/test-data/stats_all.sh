30e75a103a5021d51dfa3ca2c02e43857ce58a6a
Chad Lowe <pfmsoft@gmail.com>
2025-02-08 18:40:13 -0700

    add 2025 jan feb mar stats output

--- scripts/test-data/stats_all.sh
+++ scripts/test-data/stats_all.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 STORES_DIR="$HOME/projects/tmp/stores"
-STORE_NAME=("jan2024" "feb2024" "mar2024" "apr2024" "may2024" "jun2024" "jul2024" "aug2024" "sep2024" "oct2024" "nov2024" "dec2024")
+STORE_NAME=("jan2024" "feb2024" "mar2024" "apr2024" "may2024" "jun2024" "jul2024" "aug2024" "sep2024" "oct2024" "nov2024" "dec2024" "2025-01" "2025-02" "2025-03")
 
 for store in ${STORE_NAME[@]}; do
     pbs-parse store stats $STORES_DIR/$store
